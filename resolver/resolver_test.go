47be058de3ba008f9eb08efcc9be7b8215b710fd
acollie <alex@collie.codes>
2024-09-02 22:07:08 +0100

    chore: basic testing

--- resolver/resolver_test.go
+++ resolver/resolver_test.go
@@ -1,6 +1,18 @@
 package resolver
 
-import "testing"
+import (
+	"github.com/miekg/dns"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestHandler_DNSResolver(t *testing.T) {
+
+	t.Run("No questions in the request", func(t *testing.T) {
+		handler := Handler{}
+		resonse := dns.ResponseWriter(nil)
+		msg := new(dns.Msg)
+		err := handler.DNSResolver(resonse, msg)
+		require.Error(t, err)
+	})
 }
