842c9a592501c83d018416dcd9dfd0187c2aaac5
acollie <alex@collie.codes>
2024-09-07 23:27:17 +0100

    chore: adding basic http endpoints

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -1,6 +1,7 @@
 package resolver
 
 import (
+	"dns-server/metrcs"
 	"errors"
 	"fmt"
 	"github.com/miekg/dns"
@@ -8,17 +9,20 @@ import (
 )
 
 func (h *Handler) DNSResolver(w dns.ResponseWriter, r *dns.Msg) error {
+
+	metrcs.OpsProcessed.Inc()
 	c := new(dns.Client)
 	m := new(dns.Msg)
 
 	if len(r.Question) == 0 {
-		log.Println("No questions in the request")
+		metrcs.Failures.Inc()
 		return errors.New("no questions in the request")
 	}
 
 	for _, question := range r.Question {
 		err := h.blocklist.BlockItems.Check(question.Name)
 		if err != nil {
+			metrcs.Blocked.Inc()
 			return fmt.Errorf("blocked: %s", question.Name)
 		}
 	}
@@ -28,6 +32,7 @@ func (h *Handler) DNSResolver(w dns.ResponseWriter, r *dns.Msg) error {
 	for _, server := range h.config.Servers {
 		in, _, err = c.Exchange(m, fmt.Sprintf("%s:%d", server, dnsPort))
 		if err != nil {
+			metrcs.Failures.Inc()
 			log.Printf("Failed to query root DNS server: %v", err)
 			continue
 		}
@@ -35,6 +40,7 @@ func (h *Handler) DNSResolver(w dns.ResponseWriter, r *dns.Msg) error {
 	}
 
 	if in == nil {
+		metrcs.Failures.Inc()
 		return errors.New("all DNS queries failed")
 	}
 
